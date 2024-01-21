bd3f11807b14a3f8d13f9eb83a39192d180b971e
Hariom Singh <hariomhks1997@gmail.com>
2024-01-21 12:30:31 +0530

    light and dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,35 @@
 import Layout from "./components/Layout/Layout";
 import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Education from "./pages/Educations/Education";
 import Projects from "./pages/Projects/Projects";
 import Techstack from "./pages/Techstack/Techstack";
-
+import WorkExp from "./pages/workExp/WorkExp";
+import ScrollToTop from "react-scroll-to-top";
+import { useTheme } from "./context/ThemeContext";
 function App() {
+  const [theme] = useTheme();
   return (
     <>
-      <Layout />
-      <div className="container">
-        <About />
-        <Techstack />
-        <Projects />
+      <div id={theme}>
+        <Layout />
+        <div className="container">
+          <About />
+          <Education />
+          <Techstack />
+          <Projects />
+          <WorkExp />
+          <Contact />
+        </div>
+        <div className="footer pb-3 ms-3">
+          <h4 className="text-center">Made With 😍 Techinfoyt &copy; 2023</h4>
+        </div>
       </div>
+      <ScrollToTop
+        smooth
+        color="#f29f67"
+        style={{ backgroundColor: "#1e1e2c", borderRadius: "80px" }}
+      />
     </>
   );
 }

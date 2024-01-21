d15210b8fb4ae9fc951b4c7f388fe2dac274e32f
Hariom Singh <hariomhks1997@gmail.com>
2024-01-21 21:39:07 +0530

    deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path=require("path");
 
 //dotenv configuartion
 dotenv.config();
@@ -11,9 +12,12 @@ const app = express();
 //midlewares
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname,"./client/build")))
 //routes
 app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
+app.get("*",function(req,res){
+  res.sendFile(path.join(__dirname,"./client/build/index.html"))
+})
 
 //port
 const PORT = process.env.PORT || 8080;
